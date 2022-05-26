92e9ddeed98f74fc9b531cfd512ba17321c78e96
katia <alvarezrkatia@gmail.com>
2022-05-26 17:33:40 -0500

    Creacion de productos

---
+++ assets/js/busqueda.js
@@ -0,0 +1,120 @@
+//Ropa mujer
+
+[
+  {
+    id: 1,
+    nombre: "Docker kill",
+    tallas: "CH, MD GD EG",
+    color: "Negro",
+    descripcion: [
+      "Camiseta cómoda y suave, un básico en tu armario",
+      "Corte ajustado. Pide una talla más si prefieres un corte más holgado o ve a por una camiseta clásica.",
+      "Tela de gramaje medio: 145 g/m² (4.2 oz). Colores lisos: 100 % algodón; gris jaspeado: 90 % algodón, 10 % poliéster; antracita jaspeado: 52 % algodón, 48 % poliéster",
+    ],
+    thumbnailUrl:
+      "https://ih1.redbubble.net/image.2348964045.9669/ra,fitted_scoop,x2000,101010:01c5ca27c6,front-c,160,143,1000,1000-bg,f8f8f8.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Docker kill",
+    tallas: "CH, MD GD EG",
+    color: "Azul claro",
+    descripcion: [
+      "La camiseta tradicional de toda la vida",
+      "Corte recto, clásico y holgado",
+      "Tejido grueso de 180 g/m² de gramaje, colores lisos en algodón sanforizado. El color gris jaspeado es 90 % algodón y 10 % poliéster; el denim jaspeado es 50 % poliéster.",
+    ],
+    thumbnailUrl:
+      "https://ih1.redbubble.net/image.806592023.2159/ra,womens_tshirt,x1900,9ec0d5:0d26d5c715,front-c,140,125,1000,1000-bg,f8f8f8.jpg",
+  },
+
+  {
+    id: 3,
+    nombre: "Logotipo de JavaScript",
+    tallas: "CH, MD GD EG",
+    color: "Negro",
+    descripcion: [
+      "Camiseta cómoda y suave, un básico en tu armario",
+      "Corte ajustado. Pide una talla más si prefieres un corte más holgado o ve a por una camiseta clásica.",
+      "Tela de gramaje medio: 145 g/m² (4.2 oz). Colores lisos: 100 % algodón; gris jaspeado: 90 % algodón, 10 % poliéster; antracita jaspeado: 52 % algodón, 48 % poliéster",
+    ],
+    thumbnailUrl:
+      "https://ih1.redbubble.net/image.3019399785.0470/ra,womens_tshirt,x1900,101010:01c5ca27c6,front-c,140,125,1000,1000-bg,f8f8f8.jpg",
+  },
+
+  {
+    id: 4,
+    nombre: "Angular Camiseta clásica",
+    tallas: "CH, MD GD EG",
+    color: "Denim jaspeado",
+    descripcion: [
+      "La camiseta tradicional de toda la vida",
+      "Corte recto, clásico y holgado",
+      "Tejido grueso de 180 g/m² de gramaje, colores lisos en algodón sanforizado. El color gris jaspeado es 90 % algodón y 10 % poliéster; el denim jaspeado es 50 % poliéster.",
+    ],
+    thumbnailUrl:
+      "https://ih1.redbubble.net/image.327470741.0650/ra,womens_tshirt,x1900,322e3f:696a94a5d4,front-c,140,125,1000,1000-bg,f8f8f8.u4.jpg",
+  },
+  {
+    id: 5,
+    nombre: "GitHub - Blanco",
+    tallas: "CH, MD GD EG",
+    color: "Negro",
+    descripcion: [
+      "La camiseta tradicional de toda la vida",
+      "Corte recto, clásico y holgado",
+      "Tejido grueso de 180 g/m² de gramaje, colores lisos en algodón sanforizado. El color gris jaspeado es 90 % algodón y 10 % poliéster; el denim jaspeado es 50 % poliéster.",
+    ],
+    thumbnailUrl:
+      "https://ih1.redbubble.net/image.3592671471.5703/ssrco,chiffon_top,womens,black,front,square_three_quarter,x1000-bg,f8f8f8.1u1.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Programador NodeJS",
+    tallas: "CH, MD GD EG",
+    color: "Negro",
+    descripcion: [
+      "La camiseta tradicional de toda la vida",
+      "Corte recto, clásico y holgado",
+      "Tejido grueso de 180 g/m² de gramaje, colores lisos en algodón sanforizado. El color gris jaspeado es 90 % algodón y 10 % poliéster; el denim jaspeado es 50 % poliéster.",
+    ],
+    thumbnailUrl:
+      "https://ih1.redbubble.net/image.381286939.4001/ra,fitted_scoop,x2000,101010:01c5ca27c6,front-c,160,143,1000,1000-bg,f8f8f8.jpg",
+  },
+  {
+    id: 7,
+    nombre: "jQuery Vestido",
+    tallas: "CH, MD GD EG",
+    color: "Blanco",
+    descripcion: [
+      "Corte ancho y cómodo, con caída",
+      "Tanto la parte frontal como la trasera están totalmente estampadas con el diseño del artista independiente que hayas elegido",
+      "TTejido con tacto sedoso, fabricado en 97 % poliéster y 3 % elastano",
+    ],
+    thumbnailUrl:
+      "https://ih1.redbubble.net/image.3630417098.5150/aldr,x1440,front-c,168,326,600,600-bg,f8f8f8.u2.jpg",
+  },
+];
+
+/* 
+const playeraMujer = {
+   nombre:"Camiseta esencial",
+    tallas: "CH, MD GD EG",
+    descripcion: [ "Camiseta cómoda y suave, un básico en tu armario",
+                    "Corte ajustado. Pide una talla más si prefieres un corte más holgado o ve a por una camiseta clásica.",
+                    "Tela de gramaje medio: 145 g/m² (4.2 oz). Colores lisos: 100 % algodón; gris jaspeado: 90 % algodón, 10 % poliéster; antracita jaspeado: 52 % algodón, 48 % poliéster"
+    ],
+    color: "Negro"    
+    //img: https://ih1.redbubble.net/image.2348964045.9669/ra,fitted_scoop,x2000,101010:01c5ca27c6,front-c,160,143,1000,1000-bg,f8f8f8.jpg
+}
+
+
+const playeraMujer2 = {
+    nombre:"Docker kill",
+    tallas: "CH, MD GD EG",
+    descripcion: [ "La camiseta tradicional de toda la vida",
+                    "Tejido grueso de 180 g/m² de gramaje, colores lisos en algodón sanforizado. El color gris jaspeado es 90 % algodón y 10 % poliéster; el denim jaspeado es 50 % poliéster."
+    ],
+    color: "Azul claro"    
+    //img: https://ih1.redbubble.net/image.2348964045.9669/ra,fitted_scoop,x2000,101010:01c5ca27c6,front-c,160,143,1000,1000-bg,f8f8f8.jpg
+} */
